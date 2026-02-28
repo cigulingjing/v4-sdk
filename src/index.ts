58bbcce308ed517c84502a5d1c7c9bfb27d9797f
haogeop <haogeop@qq.com>
2026-02-28 19:27:32 +0800

    chore: export calculate liq method

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ export * from './uniswap/2-limitorder/place';
 export * from './uniswap/lib/contract';
 export * from './uniswap/lib/types';
 export * from './uniswap/lib/pool';
+export * from './uniswap/lib/liqCalculation';
 
 // Export Voucher modules
 export * from './voucher';
